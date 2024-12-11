543e9ea609486cf3cc37228399f83173fce42de4
masatokai1212 <ishome.kai@gmail.com>
2024-12-11 17:10:14 +0900

    環境変数の設定: .envファイルを更新し、LINEチャネルのシークレットとアクセストークンを追加。APIエンドポイントを実装し、LINE Messaging APIクライアントを初期化。

---
+++ api/index.ts
@@ -0,0 +1,38 @@
+import { VercelRequest, VercelResponse } from '@vercel/node';
+import { Client, WebhookRequestBody } from '@line/bot-sdk';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+// 実行時に必要なパラメータを環境変数から取得
+const config = {
+  channelSecret: process.env.CHANNEL_SECRET as string,
+  channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN as string,
+};
+
+// LINE Messaging API Clientの初期化
+const lineClient = new Client({
+  channelSecret: config.channelSecret,
+  channelAccessToken: config.channelAccessToken,
+});
+
+export default (req: VercelRequest, res: VercelResponse) => {
+  res.send("HTTP POST request sent to the webhook URL!");
+
+  // ユーザーがbotに送ったメッセージをそのまま返す
+  const { events } = req.body as WebhookRequestBody;
+  events.forEach((event) => {
+    switch (event.type) {
+      case "message": {
+        const { replyToken, message } = event;
+        if (message.type === "text") {
+          lineClient.replyMessage(replyToken, { type: "text", text: message.text });
+        }
+
+        break;
+      }
+      default:
+        break;
+    }
+  });
+};
